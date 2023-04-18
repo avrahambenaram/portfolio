e9b94182af36440a997adc2d3664a1783eafc05a
Avraham <avrahambenaramsouza@gmail.com>
2023-04-18 17:05:34 -0300

    More tecnologies

--- src/components/templates/Landing.tsx
+++ src/components/templates/Landing.tsx
@@ -9,6 +9,7 @@ import {
   DiNodejs,
   DiGit,
 } from 'react-icons/di';
+import Image from 'next/image';
 
 import { Header } from '../organisms/Header';
 import { Presentation } from '../organisms/Presentation';
@@ -16,9 +17,92 @@ import { Tecnology } from '../organisms/Tecnology';
 
 import nestjs from '../../../public/icons/nest.svg';
 import nextjs from '../../../public/icons/next.svg';
-import Image from 'next/image';
+import tailwindcss from '../../../public/icons/tailwindcss.svg';
 
 export function Landing() {
+
+  const tecnologies = [
+    {
+      title: 'HTML5',
+      complete: 1,
+      reactIcon: DiHtml5,
+    },
+    {
+      title: 'CSS',
+      complete: 1,
+      reactIcon: DiCss3,
+    },
+    {
+      title: 'Javascript',
+      complete: 1,
+      reactIcon: DiJsBadge,
+    },
+    {
+      title: 'Python',
+      complete: .8,
+      reactIcon: DiPython,
+    },
+    {
+      title: 'React',
+      complete: .9,
+      reactIcon: DiReact,
+    },
+    {
+      title: 'React Native',
+      complete: .9,
+      reactIcon: DiReact,
+    },
+    {
+      title: 'Git',
+      complete: .9,
+      reactIcon: DiGit,
+    },
+    {
+      title: 'MySQL',
+      complete: .6,
+      reactIcon: DiMysql,
+    },
+    {
+      title: 'PostgreSQL',
+      complete: .5,
+      reactIcon: DiPostgresql,
+    },
+    {
+      title: 'NodeJS',
+      complete: .9,
+      reactIcon: DiNodejs,
+    },
+    {
+      title: 'NestJS',
+      complete: .65,
+        icon: () => <Image
+        src={nestjs}
+        alt='NestJS'
+        width={36}
+        height={36}
+      />
+    },
+    {
+      title: 'NextJS',
+      complete: .75,
+        icon: () => <Image
+        src={nextjs}
+        alt='NextJS'
+        width={36}
+        height={36}
+      />
+    },
+    {
+      title: 'Tailwind CSS',
+      complete: .85,
+        icon: () => <Image
+        src={tailwindcss}
+        alt='Tailwind CSS'
+        width={36}
+        height={36}
+      />
+    },
+  ]
 
   return (
     <>
@@ -35,71 +119,15 @@ export function Landing() {
           <h3 className='text-center text-white text-xl font-semibold'>Minhas Tecnologias</h3>
           <br/>
           <div className='flex flex-wrap gap-10 justify-center'>
-            <Tecnology
-              title='HTML5'
-              complete={1}
-              ReactIcon={DiHtml5}
-            />
-            <Tecnology
-              title='CSS'
-              complete={1}
-              ReactIcon={DiCss3}
-            />
-            <Tecnology
-              title='Javascript'
-              complete={1}
-              ReactIcon={DiJsBadge}
-            />
-            <Tecnology
-              title='Python'
-              complete={.8}
-              ReactIcon={DiPython}
-            />
-            <Tecnology
-              title='React'
-              complete={.9}
-              ReactIcon={DiReact}
-            />
-            <Tecnology
-              title='Git'
-              complete={.9}
-              ReactIcon={DiGit}
-            />
-            <Tecnology
-              title='MySQL'
-              complete={.6}
-              ReactIcon={DiMysql}
-            />
-            <Tecnology
-              title='PostgreSQL'
-              complete={.5}
-              ReactIcon={DiPostgresql}
-            />
-            <Tecnology
-              title='NodeJS'
-              complete={.85}
-              ReactIcon={DiNodejs}
-            />
-            <Tecnology
-              title='NestJS'
-              complete={.65}
-              Icon={() => <Image
-                src={nestjs}
-                alt='NestJS'
-                width={36}
-                height={36}
-              />}
-            />
-            <Tecnology
-              title='NextJS'
-              complete={.65}
-              Icon={() => <Image
-                src={nextjs}
-                alt='NextJS'
-                width={36}
-                height={36}
-              />}
-            />
+            {tecnologies.map(tecnology => (
+              <Tecnology
+                key={tecnology.title}
+                title={tecnology.title}
+                complete={tecnology.complete}
+                Icon={tecnology.icon}
+                ReactIcon={tecnology.reactIcon}
+              />
+            ))}
           </div>
         </section>
         <br/>
